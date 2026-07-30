2302a460182cf2bdd020eac603143a6e6f0a371f
Igor Prado <284463586+Igorpudding@users.noreply.github.com>
2026-07-30 14:43:18 -0300

    Começo

---
+++ feira/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className='mae'>
+        <header></header>
+
+
+
+
+
+
+
+
+
+        
+      </section>
+     
+    </div>
+  );
+}
+
+export default App;
