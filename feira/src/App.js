430d7a8547bf209ed4fd2b2dc1988e6a874efacd
Igor Prado <284463586+Igorpudding@users.noreply.github.com>
2026-07-30 15:26:08 -0300

    Nice

--- feira/src/App.js
+++ feira/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import imagem from './image.png';
 
 function App() {
   return (
     <div className="App">
       <section className='mae'>
-        <header></header>
-
+      <header>
+<img src={imagem} alt='' className='logo'/> 
+      </header>
+      <section className='inc'></section>
+       
 
 
 
